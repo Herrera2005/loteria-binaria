4a884bf61c9c89ee7c1ab4bb3a05a4d855e9b048
Cristhian Herrera <criaherr@espol.edu.ec>
2026-07-25 22:54:54 -0500

    feat: cambios definitivos 2.0

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = Number(process.env.API_PORT ?? process.env.PORT ?? 3001);
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`API ejecutándose en http://localhost:${port}`);
 }
-bootstrap();
+
+void bootstrap();
